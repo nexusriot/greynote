dcc2123351d601b73eccc195a32da60e112ee66d
Vladislav Ananev <vananyev@hystax.com>
2026-02-01 00:59:22 +0400

    allow all host in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     server: {
         host: true,
         port: 5173,
+        allowedHosts: true,
         proxy: {
             "/api": "http://backend:8080",
             "/health": "http://backend:8080",
